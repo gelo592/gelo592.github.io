1c5c60c362511c35884b1d0ea725d4228aca3e80
Gracevader <gracevader@Juicez.(none)>
2014-01-17 02:04:10 -0800

    language switching?

--- js/grace.js
+++ js/grace.js
@@ -1,11 +1,26 @@
 $(document).ready(function () {
-	function switchLanguage () {
-		var langList = [];
+	var numLangs;
+	var langList = [];
+	var i = 0;  //counter that picks language from list
+
+	function setup() {
+		//pulling all different language id's
 		$(".welcomeBlock").each(function() {
 			langList.push(this.id);
 		});
-		console.log(langList);
+
+		numLangs = langList.size;
 	}
 
+	function switchLanguage () {
+		window.setTimeout(function() {
+			$('#' + langList[i]).display('none');
+			i = (i+1) % numLangs;
+			$('#' + langList[i]).display('block');
+			switchLanguage();
+		}, 5000);
+	}
+	
+	setup();
 	switchLanguage();
 });
