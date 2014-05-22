6df60f578474e7456b65b07b65090157d5ab32cf
grace e link <link@grace.cat>
2014-05-21 23:56:41 -0700

    with...the js this time

--- js/pample.js
+++ js/pample.js
@@ -5,234 +5,195 @@ canvas.height = window.innerHeight;
 
 document.body.appendChild(canvas);
 
-//create game objects
-var hero = {
-	speed: 256
-}
-
-var oracle = {}
-var oracle2 = {}
-
-var portal2 = {}
-var portal = {}
-
-//background image
-var bgReady = false;
-var bgImage = new Image();
-bgImage.onload = function () {
-	bgReady = true;
-};
-
-bgImage.src = "images/background.png";
-
-//background2 image
-var bg2Ready = false;
-var bg2Image = new Image();
-bg2Image.onload = function () {
-	bg2Ready = true;
-};
-
-bg2Image.src = "images/background2.png";
-
-//portal image
-var portalReady = false;
-var portalImage = new Image();
-portalImage.onload = new function () {
-	portalReady = true;
-	portal.height = portalImage.height;
-	portal.width = portalImage.width;
-};
-
-portalImage.src = "images/portal.png";
-
-//hero image
-var heroReady = false;
-var heroImage = new Image();
-heroImage.onload = function () {
-	heroReady = true;
-	hero.height = this.height;
-	hero.width = this.width;
-};
-
-heroImage.src = "images/hero.png";
-
-//oracle image
-var oracleReady = false;
-var oracleImage = new Image();
-oracleImage.onload = function () {
-	oracleReady = true;
-	oracle.height = this.height;
-	oracle.width = this.width;
-};
-
-oracleImage.src = "images/oracle.png";
-
-//oracle2 image
-var oracle2Ready = false;
-var oracle2Image = new Image();
-oracle2Image.onload = function () {
-	oracle2Ready = true;
-	oracle2.height = this.height;
-	oracle2.width = this.width;
-};
-
-oracle2Image.src = "images/oracle2.png";
-
-//text bubble image
-var textBubbleReady = false;
-var textBubble = new Image();
-textBubble.onload = function () {
-	textBubbleReady = true;
-};
-
-textBubble.src = "images/textbubble.png";
-
-//handle keyboard controls
-addEventListener("keydown", function(e) { update(e.keyCode, true); });
-
-function update (code, page) {
-	if (code == 37 && hero.left >= 10) { //holding left arrow
-		console.log(hero.left);
-		hero.left -= 31; 
-		hero.right = hero.left + heroImage.width;
-		render();
+function moveHero(e) {
+	if (e.code == 37 && !onEdge['left']) { //holding left arrow
+		if(!map.collisionMap[noam.x-1][noam.y]) {
+			noam.x -= 31; 
+			noam.right = noam.x + noam.image.width;
+		}	
+		else {
+			itemHit = map.itemMap[noam.x-1][noam.y];
+			itemHit.onCollide();
+		}
 	}
-	if (code == 38 && hero.top >= 10) { //holding up arrow
-		hero.top -= 23; 
-		hero.bottom = hero.top + heroImage.height;
-		render();
+	if (e.code == 38 && !onEdge['top']) { //holding up arrow
+		if(!map.collisionMap[noam.x][noam.y-1]) {
+			noam.y -= 23; 
+			noam.bottom = noam.y + noam.image.height;
+		}	
+		else {
+			itemHit = map.itemMap[noam.x][noam.y-1];
+			itemHit.onCollide();
+		}
 	}
-	if (code == 39 && hero.right <= (canvas.width - 10)) { //holding right arrow
-		hero.left += 31; 
-		hero.right = hero.left + heroImage.width;
-		render();
+	if (e.code == 39 && !onEdge['right']) { //holding right arrow
+		if(!map.collisionMap[noam.x+1][noam.y]) {
+		noam.x += 31; 
+			noam.right = noam.x + noam.image.width;
+		}	
+		else {
+			itemHit = map.itemMap[noam.x+1][noam.y];
+			itemHit.onCollide();
+		}	
 	}
-	if (code == 40 && hero.bottom <= (canvas.height - 10)) { //holding down arrow
-		hero.top += 23; 
-		hero.bottom = hero.top + heroImage.height;
-		render();
-	}
-
-	//can we talk to the oracle??
-	if (hero.left <= oracle.right
-			&& hero.right >= oracle.left
-			&& hero.top <= oracle.bottom
-			&& hero.bottom >= oracle.top && page) {
-		bubble = true;
-	}
-	else {
-		bubble = false;
-	}
-
-	//can we travel through space and time?!
-	if (hero.left <= portal.right
-			&& hero.right >= portal.left
-			&& hero.top <= portal.bottom
-			&& hero.bottom >= portal.top && page) {
-		console.log("hit p1");
-		hitPortal = true;
-	}
-	//can we travel through space and time?!
-	else if (hero.left <= portal2.right
-			&& hero.right >= portal2.left
-			&& hero.top <= portal2.bottom
-			&& hero.bottom >= portal2.top && !page) {
-		hitPortal = true;
-	}
-	else {
-		hitPortal = false;
+	if (e.code == 40 && !onEdge['bottom']) { //holding down arrow
+		if(!map.collisionMap[noam.x][noam.y+1]) {
+			noam.y += 23; 
+			noam.bottom = noam.y + noam.image.height;
+		}	
+		else {
+			itemHit = map.itemMap[noam.x][noam.y+1];
+			itemHit.onCollide();
+		}
 	}
 }
 
-var talk = function () {
-	if (textBubbleReady) {
-		ctx.drawImage(textBubble, oracle.right, oracle.top - textBubble.height + 20);
-	}
-};
+window.addEventListener('keydown', moveHero);
 
-var travelThroughTimeAndSpace = function () {
-	if (hitPortal) {
-		hitPortal = false;
-		addEventListener("keydown", function(e) { update(e.keyCode, false); });
-	}
-	else {
-		if (bg2Ready) {
-			ctx.drawImage(bg2Image, 0, 0);
-		}
+//Globals
+mapWidth = 63; //tiles
+mapHeight = 44;
 
-		if (oracle2Ready) {
-			ctx.drawImage(oracle2Image, 42, 42);
-		}
+function buildCollisionMap(mapItems) {
+	var map = [];
 
-		if (heroReady) {
-			ctx.drawImage(heroImage, hero.left, hero.top);
-		}
+	//initialize the collision map with default values
+	for (var i = mapWidth - 1; i >= 0; i--) {
+		map[i] = [];
+		for (var j = mapHeight - 1; j >= 0; j--) {
+			if(i == 0 || i == mapWidth - 1 || j == 0 || j == mapHeight - 1) {
+				map[i][j] = true;  //will cause 'collision'
+			}
+			else {
+				map[i][j] = false;  //no collision here
+			}
+		};
+	};
 
-		if (portalReady) {
-			ctx.drawImage(portalImage, portal2.left, portal2.top);
-		}
-	}
-};
+	//add each map item to the collision map
+	mapItems.forEach(function(item) {
+		map[item.x][items.y] = true;
+	});
 
-var render = function () {
-	if (hitPortal) {
-		console.log("travelingthroughsppppaaace");
-		hitPortal = false;
-		addEventListener("keydown", function(e) { update(e.keyCode, true); });
-		travelThroughTimeAndSpace();
-	}
-	else{
-		if (bgReady) {
-			ctx.drawImage(bgImage, 0, 0);
-		}
-
-		if (heroReady) {
-			ctx.drawImage(heroImage, hero.left, hero.top);
-		}
-
-		if (oracleReady) {
-			ctx.drawImage(oracleImage, oracle.left, oracle.top);
-		}
-
-		if (portalReady) {
-			ctx.drawImage(portalImage, portal.left, portal.top);
-		}
-
-		if (bubble) {
-			talk();
-		}
+	return map;
 }
+
+function buildItemMap(mapItems) {
+	var map = [];
+
+	//initialize the collision map with default values
+	for (var i = mapWidth - 1; i >= 0; i--) {
+		map[i] = [];
+		for (var j = mapHeight - 1; j >= 0; j--) {
+			if(i == 0 || i == mapWidth - 1 || j == 0 || j == mapHeight - 1) {
+				map[i][j] = 1;  //will cause 'collision' with edge
+			}
+			else {
+				map[i][j] = 0;  //no collision here
+			}
+		};
+	};
+
+	//add each map item to the collision map
+	mapItems.forEach(function(item) {
+		map[item.x][items.y] = item; //collision with an item
+	});
+}
+
+function Map (mapItems, backgroundImage, heroX, heroY) {
+	this.mapItems = mapItems;
+	this.bg = backgroundImage;
+	this.width = mapWidth;
+	this.height = mapHeight;
+	this.heroX = heroX;
+	this.heroY = heroY;
+
+	this.collisionMap = buildCollisionMap(this.mapItems);
+	this.itemMap = buildItemMap(this.mapItems);
+}
+
+Map.prototype.draw = function () { 
+	ctx.drawImage(this.bg, 0, 0);
+	for(item in this.mapItems) {
+		ctx.drawImage(item.image, item.x, item.y);
+	}
 };
 
-var init = function () {
-	hero.left = 10;
-	hero.right = hero.left + 31;
-	hero.top = 10;
-	hero.bottom = hero.top + 53;
+Map.prototype.update = function () { };
 
-	oracle.left = 382;
-	oracle.right = oracle.left + 31;
-	oracle.top = 441;
-	oracle.bottom = oracle.top + 53;
+function Hero () { 
+	this.vocab = [];
+	this.listeningLvl = 0;
+	this.readingLvl = 0;
+	this.speakingLvl = 0;
+	this.writingLvl = 0;
+	this.x = 10;
+	this.y = 10;
+	this.image = 
+}
 
-	portal.left = 606;
-	portal.right = portal.left + 101;
-	portal.top = 599;
-	portal.bottom = portal.top + 171;
+//Base class for NPC, portals, and obstacles
+function MapItem (x, y) { 
+	this.x = x;
+	this.y = y;
+}
 
-	portal2.left = 55;
-	portal2.top = 62;
-	portal2.right = portal2.left + 101;
-	portal2.bottom = portal2.top + 171;
+MapItem.prototype.onCollision = function () { alert("generic collision happening"); };
+
+//Non-human characters that our hero can interact with
+function Non_Human(x, y) { 
+	MapItem.call(x, y);
+}
+
+Non_Human.prototype = new MapItem();
+Non_Human.prototype.constructor = Non_Human;
+
+Non_Human.prototype.onCollision = function () { alert("hitting non-humans!"); };
+
+//Doorways to other worlds, dimensions, and times
+function Portal (x, y, destination) { 
+	MapItem.call(x, y);
+	this.unlocked = false;
+	this.destination = destination; //lookup key string for map object in map table
+}
+
+Portal.prototype = new MapItem();
+Portal.prototype.constructor = Portal;
+
+Portal.prototype.onCollision = function () { alert("Traveling through space and time!!"); };
+
+function Obstruction (x, y) {
+	MapItem.call(x, y);
+}
+
+Obstruction.prototype = new MapItem();
+Obstruction.prototype.constructor = Obstruction;
+
+Obstruction.prototype.onCollision = function () { alert("ouch... that doesn't do anything."); };
+
+//Setting the scenes...
+
+landingOracle = new Non_Human(382, 441);
+landingPortal = new Portal(606, 599, "jumpland");
+
+landingItems = [landingOracle, landingPortal];
+
+jumpLandOracle = new Non_Human(382, 441);
+jumpLandPortal = new Portal(606, 599, "landing");
+
+jumpLandItems = [jumpLandOracle, jumpLandPortal];
+
+landing = new Map(landingItems, "landing.png", 14, 14);
+jumpLand = new Map(jumpLandItems, "jumpland.png", 60, 60);
+
+mapTable = {
+	"jumpland": jumpLand,
+	"landing": landing
 };
 
-var then = Date.now();
-var bubble = false;
-var hitPortal = false;
-var page1 = true;
-var page2 = false;
-init();
-main = function () {
-	render();
-};
-mainInterval = setInterval(main, 1);
+onEdge = {  'left' : false,
+			'right' : false,
+			'top' : false,
+			'bottom' : false };
+
+noam = new Hero();
