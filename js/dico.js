a9ea9e657e98fd0e3b318a5083ef8dccc1a97f9e
grace e link <link@grace.cat>
2014-09-04 19:07:30 -0700

    addingcolorchangingfunctionality

--- js/dico.js
+++ js/dico.js
@@ -1,61 +1,79 @@
 google.load('search', '1');
 
-      var imageSearch;
+var imageSearch, rgbNext;
 
-      function changeTextColor() {
-        var r = Math.ceil(Math.random()*255);
-        var g = Math.ceil(Math.random()*255);
-        var b = Math.ceil(Math.random()*255);
-        var rgb = 'rgb(' + r + ', ' + g + ', ' + b + ')';
+function changeTextColor(e) {
+      var box = e.target.id;
+      var rgb, r, g, b;
+      if (box === "blackBox") {
+            r = g = b = 0;
+            rgb = 'rgb(' + r + ', ' + g + ', ' + b + ')';
+      }
+      else if (box === "whiteBox") {
+            r = g = b = 255;
+            rgb = 'rgb(' + r + ', ' + g + ', ' + b + ')';
+      }
+      else if (box === "colorBox") { 
+            var rgb = $(this).css('color');
 
-        $('input').css('color', rgb);
-        $('.textPos').css('color', rgb);
+            r = Math.ceil(Math.random()*255);
+            g = Math.ceil(Math.random()*255);
+            b = Math.ceil(Math.random()*255);
+            $(this).css('color', 'rgb(' + r + ', ' + g + ', ' + b + ')');
+
+
       }
 
-      function searchComplete() {
+      $('input').css('color', rgb);
+      $('.textPos').css('color', rgb);
+}
 
-        // Check that we got results
-        if (imageSearch.results && imageSearch.results.length > 0) {
+function searchComplete() {
 
-          // Grab our content div, clear it.
-          var contentDiv = $('#content');
+      // Check that we got results
+      if (imageSearch.results && imageSearch.results.length > 0) {
 
-          // Loop through our results, printing them to the page.
-          var results = imageSearch.results;
-          var result = results[0];
+            // Grab our content div, clear it.
+            var contentDiv = $('#content');
 
-          var newImg = $('#background');
-          console.log(result.url);
-          // There is also a result.url property which has the escaped version
-          newImg.css('background-image', 'url(' + result.url + ')');
-        }
+            // Loop through our results, printing them to the page.
+            var results = imageSearch.results;
+            var result = results[0];
+
+            var newImg = $('#background');
+            console.log(result.url);
+            // There is also a result.url property which has the escaped version
+            newImg.css('background-image', 'url(' + result.url + ')');
       }
+}
 
-      function OnLoad(e) {
-        console.log(e.keyCode);
-        if(e.keyCode == 13) {
-          var inputText = document.getElementById("word").value;
-          console.log(inputText);
-          // Create an Image Search instance.
-          imageSearch = new google.search.ImageSearch();
+function OnLoad(e) {
+      console.log(e.keyCode);
+      if(e.keyCode == 13) {
+            var inputText = document.getElementById("word").value;
+            console.log(inputText);
+            // Create an Image Search instance.
+            imageSearch = new google.search.ImageSearch();
 
-          // Set searchComplete as the callback function when a search is 
-          // complete.  The imageSearch object will have results in it.
-          imageSearch.setSearchCompleteCallback(this, searchComplete, null);
+            // Set searchComplete as the callback function when a search is 
+            // complete.  The imageSearch object will have results in it.
+            imageSearch.setSearchCompleteCallback(this, searchComplete, null);
 
 
-          // Find me a beautiful car.
-          imageSearch.execute(inputText);
-          
-          // Include the required Google branding
+            // Find me a beautiful car.
+            imageSearch.execute(inputText);
 
-        }
+            // Include the required Google branding
+
       }
+}
 
-      function setHandler() {
-        window.addEventListener('keydown', OnLoad);
-        $('#colorBox').addEventListener('click', changeTextColor);
-        google.search.Search.getBranding('branding');
-      }
+function setHandler() {
+      window.addEventListener('keydown', OnLoad);
+      $('#colorBox').click(changeTextColor);
+      $('#whiteBox').click(changeTextColor);
+      $('#blackBox').click(changeTextColor);
+      google.search.Search.getBranding('branding');
+}
 
-      google.setOnLoadCallback(setHandler);
+google.setOnLoadCallback(setHandler);
