14cd4f6368379a6f532764e50c81f98aa633b271
g.e. link <link@grace.cat>
2015-08-25 16:55:56 -0500

    maybe im more responsive now question mark

--- popsite/js/main.js
+++ popsite/js/main.js
@@ -5,7 +5,12 @@
 
 $(function() {
   function attachListeners() {
-    $(window).on("scroll resize", scrollyWatch);
+    if($("#myCarousel").length > 0) {
+      $(window).resize(function() {
+        var h = $("#myCarousel").height();
+        $(".info-blob").height(h*.9);
+      });
+    }
 
     $(".playlist").on("click", "tr.clickable", function(e) {
       e = e.currentTarget;
@@ -20,6 +25,7 @@ $(function() {
       $("tr.active").removeClass("active");
       $(e).addClass("active");
     });
+
     $(".playlist").on("dblclick", "tr.clickable", function(e) {
       e = e.currentTarget;
 
@@ -39,35 +45,6 @@ $(function() {
       e.preventDefault()
       $(this).tab('show')
     })
-  }
-
-  function scrollyWatch() {
-    var scrollHead = $('#scroll-head');
-    var height = scrollHead.outerHeight();
-
-    var berries = $('#berries').offset().top;
-    //var end = $('#scroll-head-end').offset().top;
-    var dirty = $('#dirty').offset().top;
-    //var clean = $('#clean').offset().top;
-    var scroll = (window).scrollY;
-
-    if(scroll >= berries - height && scroll < dirty - height)
-    {
-      //$('#scroll-head h3')[0].innerHTML = 'Foods to Eat Often';
-      $('#scroll-head').addClass('scroll-heading');
-    }
-/*  ADD THIS BACK WHEN BOTTOM PICTURES DONT LOOK LIKE TURDS
-    else if(scroll >= dirty - height && scroll < clean - height) {
-      $('#scroll-head h3')[0].innerHTML = 'Dirty Dozen';
-      $('#scroll-head').addClass('scroll-heading');
-    }
-    else if(scroll >= clean - height) {
-      $('#scroll-head h3')[0].innerHTML = 'Clean Fifteen';
-      $('#scroll-head').addClass('scroll-heading');
-    } */
-    else {
-      $('#scroll-head').removeClass('scroll-heading');
-    }
   }
 
   attachListeners();
