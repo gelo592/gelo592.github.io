0eb7f0c10789e9644bc9a56a8db1ca8d9e3d6e1a
g.e. link <link@grace.cat>
2017-01-12 21:33:47 +0800

    better kibodo

--- js/kibodo.js
+++ js/kibodo.js
@@ -1,25 +1,34 @@
 var kibodo = {
+  el: $("#kibodo"),
   controls: false,
   autoplay: false,
   loop: false,
-  currentClip: "",
+  stackEffects: false,
+  currentClip: 1,
   currentHue: 0,
   currentPlaybackRate: 1,
+  currentPresetFunction: 'setHue',
+  reverse: false,
+  paused: false,
+  global: false,
 
   play: function() {
-    return true;
+    this.paused = false;
+    $("#vid"+this.currentClip)[0].play();
+    $("#vid"+this.currentClip).removeClass("paused");
   },
 
   pause: function() {
-    return true;
-
+    this.paused = true;
+    $("#vid"+this.currentClip)[0].pause();
+    $("#vid"+this.currentClip).addClass("paused");
   },
 
   seek: function() {
 
   },
 
-  currentTiem: function () {
+  currentTime: function () {
     return true;
 
   },
@@ -27,6 +36,17 @@ var kibodo = {
   duration: function() {
     return true;
 
+  },
+
+  togglePlayPause: function() {
+    if(this.paused) {
+    $(".pause-key").removeClass("key-pressed-hold");
+      this.play();
+    }
+    else {
+    $(".pause-key").addClass("key-pressed-hold");
+      this.pause();
+    }
   },
 
   toggleLoop: function() {
@@ -45,73 +65,113 @@ var kibodo = {
   },
 
   changePlaybackSpeed: function(rate) {
-    console.log("here");
     var newRate = this.currentPlaybackRate + parseFloat(rate);
     this.currentPlaybackRate = newRate < .25 ? .25 : newRate > 3 ? 3 : newRate;
-    console.log("boop");
-    console.log(this.currentPlaybackRate);
-    document.getElementById("vid").playbackRate = this.currentPlaybackRate;
+    $("#vid"+this.currentClip)[0].playbackRate = this.currentPlaybackRate;
+  },
+
+  changePreset: function() {
+
+  },
+
+  applyPreset: function(value) {
+    //this.currentPresetFunction(value);
+
+    if(this.currentPresetFunction == "setHue") {
+      this.setHue(value);
+    }
+  },
+
+  startStopRecord: function() {
+
   },
 
   clearFilters: function() {
     this.currentHue = 0;
     this.invert = false;
     this.greyscale = false;
-    $("#vid").css("filter", "");
+    $("#vid"+this.currentClip).css("filter", "");
   },
 
   rotateHue: function(hue) {
     var newHue = this.currentHue + hue;
 
-    this.clearFilters();
+    if(!this.stackEffects){this.clearFilters()};
     this.currentHue = newHue;
 
-    $("#vid").css("filter", "hue-rotate(" + newHue + "deg)");
+    $("#vid"+this.currentClip).css("filter", "hue-rotate(" + newHue + "deg)");
   },
 
   setHue: function(hue) {
-    this.clearFilters();
+    if(!this.stackEffects){this.clearFilters()};
     this.currentHue = hue;
 
-    $("#vid").css("filter", "hue-rotate(" + hue + "deg)");
+    $("#vid"+this.currentClip).css("filter", "hue-rotate(" + hue + "deg)");
   },
 
   toggleGrey: function() {
     var tempGrey = !this.greyscale;
 
-    this.clearFilters();
+    if(!this.stackEffects){this.clearFilters()};
     this.greyscale = tempGrey;
 
     if(this.greyscale) {
-      $("#vid").css("filter", "grayscale(1)");
+      $("#vid"+this.currentClip).css("filter", "grayscale(1)");
     }
     else {
-      $("#vid").css("filter", "grayscale(0)");
+      $("#vid"+this.currentClip).css("filter", "grayscale(0)");
     }
   },
 
   toggleInvert: function() {
     var tempInvert = !this.invert;
 
-    this.clearFilters();
+    if(!this.stackEffects){this.clearFilters()};
     this.invert = tempInvert;
 
     if(this.invert) {
-      $("#vid").css("filter", "invert(1)");
+      $("#vid"+this.currentClip).css("filter", "invert(1)");
     }
     else {
-      $("#vid").css("filter", "invert(0)");
+      $("#vid"+this.currentClip).css("filter", "invert(0)");
     }
   },
 
-  setClip: function(clip) {
-    //if(clip != this.currentClip) {
-      this.currentClip = clip;
-      $("#vid")[0].src = "clips/" + clip + ".mp4";
-    /*}
+  toggleReverse: function() {
+    var tempReverse = !this.reverse;
+    if(!this.stackEffects){this.clearFilters()};
+    this.reverse = tempReverse;
+
+    if(this.reverse) {
+      $(".reverse-key").addClass("key-pressed-hold");
+    }
     else {
-      //seek to beginning
-    }*/
+      $(".reverse-key").removeClass("key-pressed-hold");
+    }
+
+    this.setClip(this.currentClip);
+  },
+
+  toggleGlobalMode: function() {
+    this.global = !this.global;
+  },
+
+  setClip: function(clip) {
+    var clipTag = this.reverse ? clip + 'r' : clip;
+    if(clip != this.currentClip) {
+      this.currentClip = clip;
+      $(".video").addClass('video-hid');
+      $("#vid"+clipTag).removeClass('video-hid');
+
+      if($("#vid"+clipTag).hasClass("paused")) {
+        this.paused = true;
+        $(".pause-key").addClass("key-pressed-hold");
+      }
+      else {
+        this.paused = false;
+        $(".pause-key").removeClass("key-pressed-hold");
+      }
+    }
   }
 };
 
@@ -122,12 +182,7 @@ $(window).keyup(function(e){
     var key = $(e).data('key');
 
     if(key == keyVal) {
-      if($(e).hasClass("big-key")) {
-        $("#"+e.id).css("background-position-x", -24);
-      }
-      else {
-        $("#"+e.id).css("background-position-y", 0);
-      }
+      $(e).removeClass("key-pressed");
     }
   });
 });
@@ -140,17 +195,9 @@ $(window).keydown(function(e){
     var action = $(e).data('function');
     var data = $(e).data('keystuffs');
 
-    console.log(action);
-
     if(key == keyVal) {
       kibodo[action](data);
-
-      if($(e).hasClass("big-key")) {
-        $("#"+e.id).css("background-position-x", 0);
-      }
-      else {
-        $("#"+e.id).css("background-position-y", -24);
-      }
+      $(e).addClass("key-pressed");
     }
   });
 });
